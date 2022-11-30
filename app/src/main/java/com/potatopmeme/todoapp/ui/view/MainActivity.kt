e620f6678318110dc59c5130d472906c23e007d5
KimSeongHwan <89847975+PotatoMeme@users.noreply.github.com>
2022-11-30 17:50:50 +0900

    Set "베이스 세팅 - 1,[BuildGradle,아이콘,이미지,페키지]"

--- app/src/main/java/com/potatopmeme/todoapp/ui/view/MainActivity.kt
+++ app/src/main/java/com/potatopmeme/todoapp/ui/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.potatopmeme.todoapp
+package com.potatopmeme.todoapp.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.potatopmeme.todoapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

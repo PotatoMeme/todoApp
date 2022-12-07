e3cad01d01694002082ded60fa850ae6d984725e
KimSeongHwan <89847975+PotatoMeme@users.noreply.github.com>
2022-12-07 15:26:09 +0900

    Feat StartAct

---
+++ app/src/main/java/com/potatopmeme/todoapp/ui/view/StartActivity.kt
@@ -0,0 +1,23 @@
+package com.potatopmeme.todoapp.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.potatopmeme.todoapp.R
+import com.potatopmeme.todoapp.databinding.ActivityStartBinding
+
+class StartActivity : AppCompatActivity() {
+
+    private var _binding: ActivityStartBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding =
+            DataBindingUtil.setContentView(this, R.layout.activity_start)
+        binding.floatingActionButton.setOnClickListener {
+            finish()
+        }
+    }
+}

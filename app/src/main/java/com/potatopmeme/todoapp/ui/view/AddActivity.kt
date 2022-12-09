ed3f06d7ef72768b2c6037fb40b88b90bbd4177b
PotatoMeme <kshk0897@gmail.com>
2022-12-09 10:14:35 +0900

    Feat Add(TimeDialog,SpinnerToLayout)

--- app/src/main/java/com/potatopmeme/todoapp/ui/view/AddActivity.kt
+++ app/src/main/java/com/potatopmeme/todoapp/ui/view/AddActivity.kt
@@ -1,8 +1,8 @@
 package com.potatopmeme.todoapp.ui.view
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.format.Time
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
@@ -16,7 +16,7 @@ import com.potatopmeme.todoapp.databinding.ActivityAddBinding
 class AddActivity : AppCompatActivity() {
     private var _binding: ActivityAddBinding? = null
     private val binding get() = _binding!!
-    private var arrWeek = arrayOf(false,false,false,false,false,false,false)
+    private var arrWeek = arrayOf(false, false, false, false, false, false, false)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,106 +28,129 @@ class AddActivity : AppCompatActivity() {
             finish()
         }
 
+        setupTimeLayout()
+        setupSpinner()
+
+        setupWeek()
+
+        setFirstLayoutSet()
+    }
+
+    private fun setupTimeLayout() {
         binding.scTime.setOnCheckedChangeListener { _, b ->
-            var layoutParms = binding.timeLayout.layoutParams
+            var layoutParams = binding.timeLayout.layoutParams
             when (b) {
                 true -> {
-                    layoutParms.height = LinearLayout.LayoutParams.WRAP_CONTENT
-                    binding.timeLayout.layoutParams = layoutParms
+                    layoutParams.height = LinearLayout.LayoutParams.WRAP_CONTENT
+                    binding.timeLayout.layoutParams = layoutParams
                 }
                 false -> {
-                    layoutParms.height = 0
-                    binding.timeLayout.layoutParams = layoutParms
+                    layoutParams.height = 0
+                    binding.timeLayout.layoutParams = layoutParams
                 }
             }
         }
-        setupWeek()
-        setupSpinner()
+
+        binding.btnTime.setOnClickListener {
+            Log.d(TAG, "setupTimeLayout: btnTime clicked")
+            val dlg = TimeDialog(this)
+            dlg.setOnOKClickedListener { time ->
+                binding.etTime.setText(time)
+            }
+            dlg.show()
+        }
+    }
+
+    private fun setFirstLayoutSet() {
+        setDateLayoutHeight(LinearLayout.LayoutParams.WRAP_CONTENT)
+        setWeekLayoutHeight(0)
+        setDurationLayoutHeight(0)
+        setDatesLayoutHeight(0)
     }
 
     private fun setupWeek() {
-        val colorDarkGray : Int = ContextCompat.getColor(this,R.color.base_dark_gray)
-        val colorWhite : Int = ContextCompat.getColor(this,R.color.white)
-        val colorBlack : Int = ContextCompat.getColor(this,R.color.black)
+        val colorDarkGray: Int = ContextCompat.getColor(this, R.color.base_dark_gray)
+        val colorWhite: Int = ContextCompat.getColor(this, R.color.white)
+        val colorBlack: Int = ContextCompat.getColor(this, R.color.black)
 
-        binding.btnSun.setOnClickListener{
-            if (arrWeek[0]){
+        binding.btnSun.setOnClickListener {
+            if (arrWeek[0]) {
                 binding.btnSun.setCardBackgroundColor(colorWhite)
                 binding.tvSun.setTextColor(colorBlack)
                 arrWeek[0] = false
-            }else{
+            } else {
                 binding.btnSun.setCardBackgroundColor(colorDarkGray)
                 binding.tvSun.setTextColor(colorWhite)
                 arrWeek[0] = true
             }
         }
 
-        binding.btnMon.setOnClickListener{
-            if (arrWeek[1]){
+        binding.btnMon.setOnClickListener {
+            if (arrWeek[1]) {
                 binding.btnMon.setCardBackgroundColor(colorWhite)
                 binding.tvMon.setTextColor(colorBlack)
                 arrWeek[1] = false
-            }else{
+            } else {
                 binding.btnMon.setCardBackgroundColor(colorDarkGray)
                 binding.tvMon.setTextColor(colorWhite)
                 arrWeek[1] = true
             }
         }
 
-        binding.btnTue.setOnClickListener{
-            if (arrWeek[2]){
+        binding.btnTue.setOnClickListener {
+            if (arrWeek[2]) {
                 binding.btnTue.setCardBackgroundColor(colorWhite)
                 binding.tvTue.setTextColor(colorBlack)
                 arrWeek[2] = false
-            }else{
+            } else {
                 binding.btnTue.setCardBackgroundColor(colorDarkGray)
                 binding.tvTue.setTextColor(colorWhite)
                 arrWeek[2] = true
             }
         }
 
-        binding.btnWed.setOnClickListener{
-            if (arrWeek[3]){
+        binding.btnWed.setOnClickListener {
+            if (arrWeek[3]) {
                 binding.btnWed.setCardBackgroundColor(colorWhite)
                 binding.tvWed.setTextColor(colorBlack)
                 arrWeek[3] = false
-            }else{
+            } else {
                 binding.btnWed.setCardBackgroundColor(colorDarkGray)
                 binding.tvWed.setTextColor(colorWhite)
                 arrWeek[3] = true
             }
         }
 
-        binding.btnThu.setOnClickListener{
-            if (arrWeek[4]){
+        binding.btnThu.setOnClickListener {
+            if (arrWeek[4]) {
                 binding.btnThu.setCardBackgroundColor(colorWhite)
                 binding.tvThu.setTextColor(colorBlack)
                 arrWeek[4] = false
-            }else{
+            } else {
                 binding.btnThu.setCardBackgroundColor(colorDarkGray)
                 binding.tvThu.setTextColor(colorWhite)
                 arrWeek[4] = true
             }
         }
 
-        binding.btnFri.setOnClickListener{
-            if (arrWeek[5]){
+        binding.btnFri.setOnClickListener {
+            if (arrWeek[5]) {
                 binding.btnFri.setCardBackgroundColor(colorWhite)
                 binding.tvFri.setTextColor(colorBlack)
                 arrWeek[5] = false
-            }else{
+            } else {
                 binding.btnFri.setCardBackgroundColor(colorDarkGray)
                 binding.tvFri.setTextColor(colorWhite)
                 arrWeek[5] = true
             }
         }
 
-        binding.btnSat.setOnClickListener{
-            if (arrWeek[6]){
+        binding.btnSat.setOnClickListener {
+            if (arrWeek[6]) {
                 binding.btnSat.setCardBackgroundColor(colorWhite)
                 binding.tvSat.setTextColor(colorBlack)
                 arrWeek[6] = false
-            }else{
+            } else {
                 binding.btnSat.setCardBackgroundColor(colorDarkGray)
                 binding.tvSat.setTextColor(colorWhite)
                 arrWeek[6] = true
@@ -148,6 +171,32 @@ class AddActivity : AppCompatActivity() {
                 id: Long
             ) {
                 Log.d(TAG, "onItemSelected: $position")
+                when (position) {
+                    0 -> {
+                        setDateLayoutHeight(LinearLayout.LayoutParams.WRAP_CONTENT)
+                        setWeekLayoutHeight(0)
+                        setDurationLayoutHeight(0)
+                        setDatesLayoutHeight(0)
+                    }
+                    1 -> {
+                        setDateLayoutHeight(0)
+                        setWeekLayoutHeight(LinearLayout.LayoutParams.WRAP_CONTENT)
+                        setDurationLayoutHeight(0)
+                        setDatesLayoutHeight(0)
+                    }
+                    2 -> {
+                        setDateLayoutHeight(0)
+                        setWeekLayoutHeight(0)
+                        setDurationLayoutHeight(LinearLayout.LayoutParams.WRAP_CONTENT)
+                        setDatesLayoutHeight(0)
+                    }
+                    3 -> {
+                        setDateLayoutHeight(0)
+                        setWeekLayoutHeight(0)
+                        setDurationLayoutHeight(0)
+                        setDatesLayoutHeight(LinearLayout.LayoutParams.WRAP_CONTENT)
+                    }
+                }
 
             }
 
@@ -155,7 +204,30 @@ class AddActivity : AppCompatActivity() {
             }
         }
 
+    }
 
+    private fun setDateLayoutHeight(height: Int) {
+        var layoutParams = binding.dateLayout.layoutParams
+        layoutParams.height = height
+        binding.dateLayout.layoutParams = layoutParams
+    }
+
+    private fun setWeekLayoutHeight(height: Int) {
+        var layoutParams = binding.weekLayout.layoutParams
+        layoutParams.height = height
+        binding.weekLayout.layoutParams = layoutParams
+    }
+
+    private fun setDurationLayoutHeight(height: Int) {
+        var layoutParams = binding.durationLayout.layoutParams
+        layoutParams.height = height
+        binding.durationLayout.layoutParams = layoutParams
+    }
+
+    private fun setDatesLayoutHeight(height: Int) {
+        var layoutParams = binding.datesLayout.layoutParams
+        layoutParams.height = height
+        binding.datesLayout.layoutParams = layoutParams
     }
 
     companion object {
---
+++ app/src/main/java/com/potatopmeme/todoapp/ui/view/TimeDialog.kt
@@ -0,0 +1,49 @@
+package com.potatopmeme.todoapp.ui.view
+
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import com.potatopmeme.todoapp.databinding.DialogTimepickerBinding
+
+class TimeDialog(private val context :AppCompatActivity) {
+    private lateinit var binding :DialogTimepickerBinding
+    private val dlg = Dialog(context)
+
+    private lateinit var listener : TimeDialogOKClickedListener
+
+    fun show(){
+        binding = DialogTimepickerBinding.inflate(context.layoutInflater)
+
+        dlg.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dlg.setContentView(binding.root)
+        //dlg.setCancelable(false)
+        dlg.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+
+        binding.tvOk.setOnClickListener {
+            listener.onOKClicked("${binding.timePicker.hour} : ${binding.timePicker.minute}")
+            dlg.dismiss()
+        }
+
+        binding.tvCancel.setOnClickListener {
+            dlg.dismiss()
+        }
+
+        dlg.show()
+    }
+
+    fun setOnOKClickedListener(listener: (String) -> Unit) {
+        this.listener = object: TimeDialogOKClickedListener {
+            override fun onOKClicked(content: String) {
+                listener(content)
+            }
+        }
+    }
+
+
+    interface TimeDialogOKClickedListener {
+        fun onOKClicked(content : String)
+    }
+}
